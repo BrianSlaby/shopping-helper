e5d05b03a27c4262fd5c81aaa50650c45b2c4684
Brian Slaby <brian.slaby@gmail.com>
2024-01-10 16:56:51 -0500

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ export default function App() {
 
   return (
     <>
-      
+      <header>
+        <h1>Shopping Helper</h1>
+        <img src="./public/cat.png" alt=""/>
+      </header>
     </>
   )
 }
--- src/components/AuthRequired.jsx
+++ src/components/AuthRequired.jsx
@@ -4,7 +4,9 @@ export default function AuthRequired() {
 
     return (
         <>
-            
+            <div className="login-container">
+                
+            </div>
         </>
     )
 }
